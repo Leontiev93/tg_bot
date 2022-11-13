fa450af7151d5710243947ffbdf4cb9a52c3f4b2
Герман Леонтьев <gerleonsto@gmail.com>
2022-11-14 00:22:14 +0300

    Ver1

--- bot_anton/tgc/admin.py
+++ bot_anton/tgc/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import ProfileForm
+from .models import Profile, Massage
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'external_id', 'name')
+    form = ProfileForm
+
+
+@admin.register(Massage)
+class MassageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'profile', 'text', 'created_at')
